481ae9afe7d2b237c23762741c049dca1a5569a6
Andrés B <andres.b.dev@gmail.com>
2012-01-05 23:10:56 -0300

    Some fixes

--- Rakefile
+++ Rakefile
@@ -1,1 +1,12 @@
 require "bundler/gem_tasks"
+require 'rake/testtask'
+require 'rspec'
+require 'rspec/core/rake_task'
+
+
+task :default => :spec
+task :test => :spec
+
+RSpec::Core::RakeTask.new(:spec) do
+  system "echo \"recreating database \" && cd #{File.join(File.dirname(__FILE__), 'spec', 'app')} && rake db:migrate:reset"
+end 
--- lib/generators/extend_at/extend_at.rb
+++ lib/generators/extend_at/extend_at.rb
@@ -1,18 +1,17 @@
+require 'rails/generators/base'
+
 module ExtendAt
-#   module Generators
+  module Generators
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
       source_root File.expand_path('./templates', __FILE__)
       desc "Generate all necesaries models and migrations for extend_at gem"
-      def install
-        create_model_file
-      end
 
       def self.next_migration_number(path)
         Time.now.utc.strftime("%Y%m%d%H%M%S")
       end
 
-      def create_model_file
+      def create_model
         template "integer_value.rb", "app/models/integer_value.rb"
         template "float_value.rb", "app/models/float_value.rb"
         template "string_value.rb", "app/models/string_value.rb"
@@ -21,5 +20,5 @@ module ExtendAt
         migration_template "create_extend_at_tables.rb", "db/migrate/create_extend_at_tables.rb"
       end
     end
-#   end
+  end
 end
