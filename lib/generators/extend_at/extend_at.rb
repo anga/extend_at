36ba290303d3c88fa5fe69aceaa31dcd215bdd1b
Andrés B <andres.b.dev@gmail.com>
2012-01-05 23:09:16 -0300

    Majors changes, changed the "core" work. WARNING: not work for now

--- lib/generators/extend_at/extend_at.rb
+++ lib/generators/extend_at/extend_at.rb
@@ -1,9 +1,12 @@
 module ExtendAt
-  module Generators
-    class InstallGenerator < Rails::Generators::NamedBase
+#   module Generators
+    class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
       source_root File.expand_path('./templates', __FILE__)
       desc "Generate all necesaries models and migrations for extend_at gem"
+      def install
+        create_model_file
+      end
 
       def self.next_migration_number(path)
         Time.now.utc.strftime("%Y%m%d%H%M%S")
@@ -18,5 +21,5 @@ module ExtendAt
         migration_template "create_extend_at_tables.rb", "db/migrate/create_extend_at_tables.rb"
       end
     end
-  end
+#   end
 end
